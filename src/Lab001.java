179b89fa8c5514d2d498fcf645a6dcb4c693b4b4
Sanjeev <82.sanjeev@gmail.com>
2024-01-14 21:30:25 +0530

    changes done in output

--- src/Lab001.java
+++ src/Lab001.java
@@ -2,6 +2,6 @@ package src;
 
 public class Lab001 {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello welcome to Automation batch");
     }
 }
