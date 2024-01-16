fa9ada74a564e7147be4d9ec87a5e29cde477304
Sanjeev <82.sanjeev@gmail.com>
2024-01-16 22:35:31 +0530

    changes done

---
+++ src/basic_02/Lab004.java
@@ -0,0 +1,13 @@
+package src.basic_02;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        char c='A';
+        System.out.println(c);
+     //   char c1 ="$"; // string
+     char c1 ='#';
+        System.out.println(c1);
+        char c2='\n';
+        System.out.println(c2);
+    }
+}
