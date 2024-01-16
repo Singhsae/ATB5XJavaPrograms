4bb2ea6956ac34511a93571dc6e1af384f025937
Sanjeev <82.sanjeev@gmail.com>
2024-01-16 23:29:13 +0530

    new class added

--- src/basic_02/Lab007.java
+++ src/basic_02/Lab007.java
@@ -2,6 +2,17 @@ package src.basic_02;
 
 public class Lab007 {
     public static void main(String[] args) {
+        char c='\n';//next line
+        char c1='\t';//add tab
+        char c2='\b';//backlash , delete 1 char
+        char c3='\r';
+        System.out.println("Sanjeev"+c3+"singh");
+        System.out.println("Sanjeev"+c1+"Singh");
+        System.out.println("Sanjeev"+c2+"Kumar");
+        System.out.println("Sanjeev"+c2+"Singh");
+
+
+
 
     }
 }
---
+++ src/basic_02/Lab008.java
@@ -0,0 +1,9 @@
+package src.basic_02;
+
+public class Lab008 {
+    public static void main(String[] args) {
+        byte a=10;
+        byte b=18;
+        System.out.println(a+b);
+    }
+}
