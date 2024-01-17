52bc99cd29a1f2cd575a517c14d0b18613c0718e
Sanjeev <82.sanjeev@gmail.com>
2024-01-17 22:37:46 +0530

    new package added

---
+++ src/basic_03/Lab009.java
@@ -0,0 +1,13 @@
+package src.basic_03;
+
+public class Lab009 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=45;
+        System.out.println(a+b);
+        String name ="sanjeev";
+        System.out.println(a+name);
+        System.out.println(a+b+name);
+        System.out.println(name+a+b);
+    }
+}
