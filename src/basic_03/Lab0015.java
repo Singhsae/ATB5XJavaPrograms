123e29c6398e1cb8f78105c070061439271cb951
Sanjeev <82.sanjeev@gmail.com>
2024-01-17 23:31:41 +0530

    new programs added

--- src/basic_03/Lab0015.java
+++ src/basic_03/Lab0015.java
@@ -1,4 +1,13 @@
 package src.basic_03;
 
 public class Lab0015 {
+    public static void main(String[] args) {
+        int a =12;
+        boolean b= a>10 || a<5;
+        System.out.println(b);
+         int a1=90;
+         int b1=90;
+         boolean c=(a1<=b1);
+        System.out.println(c);
+    }
 }
