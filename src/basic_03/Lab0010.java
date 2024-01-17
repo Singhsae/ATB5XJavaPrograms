27d87838a84d7ca0c01d6aa784bf88d88d3bd33e
Sanjeev <82.sanjeev@gmail.com>
2024-01-17 22:52:27 +0530

    few changes done

--- src/basic_03/Lab0010.java
+++ src/basic_03/Lab0010.java
@@ -1,2 +1,15 @@
-package src.basic_03;public class Lab0010 {
+package src.basic_03;
+
+public class Lab0010 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=45;
+        b+=30;
+        System.out.println(b);
+         int b1 =90;
+         b1-=89;
+         b1=b1+1;
+        System.out.println(b1);
+
+    }
 }
--- src/basic_03/Lab0011.java
+++ src/basic_03/Lab0011.java
@@ -1,2 +1,16 @@
-package src.basic_03;public class Lab0011 {
+package src.basic_03;
+
+public class Lab0011 {
+    public static void main(String[] args) {
+        //Relational operators
+        int age_mamitha=30;
+        int age_Pramod=35;
+        boolean result = age_Pramod>age_mamitha;
+        System.out.println(result);
+        int a=10;
+        int b=10;
+        boolean c=(a==b);
+        System.out.println(c);
+        System.out.println(a>b);
+    }
 }
