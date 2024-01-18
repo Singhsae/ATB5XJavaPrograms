256924c1805d8f8eb694f1889c79b94e5ae7ab71
Sanjeev <82.sanjeev@gmail.com>
2024-01-18 22:39:27 +0530

    new programs added

---
+++ src/basic_03/Lab0016.java
@@ -0,0 +1,11 @@
+package src.basic_03;
+
+public class Lab0016 {
+    public static void main(String[] args) {
+        String s1="Sanjeev";
+
+        String s2= new String("Sanjeev");
+        boolean s3 =s1==s2;
+        System.out.println(s3);
+    }
+}
