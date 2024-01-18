21afca2114ceeca9c2ad576aee8598717d90cff9
Sanjeev <82.sanjeev@gmail.com>
2024-01-19 00:25:59 +0530

    new programs added

--- src/basic_03/Lab0025.java
+++ src/basic_03/Lab0025.java
@@ -10,12 +10,12 @@ public class Lab0025 {
         int a =sc.nextInt();
         int b= sc.nextInt();
 
-        if (a>b){
-            System.out.println("Max" +a);
+        if (a<b){
+            System.out.println("Min" +a);
 
         }
         else{
-            System.out.println("Max" +b);
+            System.out.println("Min" +b);
         }
 
     }
