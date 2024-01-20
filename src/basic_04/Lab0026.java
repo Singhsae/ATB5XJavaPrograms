d241976f1353741cb753d4f79f6bb55312047791
Sanjeev <82.sanjeev@gmail.com>
2024-01-20 22:50:57 +0530

    New Program

--- src/basic_04/Lab0026.java
+++ src/basic_04/Lab0026.java
@@ -1,4 +1,23 @@
 package src.basic_04;
 
+import java.util.Scanner;
+
 public class Lab0026 {
-}
+    //Grade Calculator
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the number");
+          int a= sc.nextInt();
+        if(a>20) {
+            System.out.println("Number is greater than " + a);
+        } else if (a>10) {
+            System.out.println("Number is greater than 10");
+        }
+          else  {
+                System.out.println("Number is less than 20 and 10");
+
+        }
+
+    }
+    }
+
