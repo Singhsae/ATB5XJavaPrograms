34dcfb62926637a1937ef267dd92b19ffd7e7fed
Sanjeev <82.sanjeev@gmail.com>
2024-01-20 23:12:51 +0530

    some text changes

--- src/basic_04/Lab0027.java
+++ src/basic_04/Lab0027.java
@@ -15,16 +15,16 @@ public class Lab0027 {
         int score = sc.nextInt();
         //char grade='F';
         if (score>=90 && score <=100){
-            System.out.println("You scored grade A ");
+            System.out.println("Your grade is : A ");
         } else if (score>=80 && score<=89) {
-            System.out.println("You scored grade B");
+            System.out.println("Your grade is : B");
         } else if (score>=70 && score<=79) {
-            System.out.println("You scored grade C");
+            System.out.println("Your grade is : C");
         } else if (score>=60 && score <=69) {
-            System.out.println("You scored grade D");
+            System.out.println("Your grade is : D");
         }
         else {
-            System.out.println("You scored F");
+            System.out.println("Your grade is : F");
         }
 
     }
