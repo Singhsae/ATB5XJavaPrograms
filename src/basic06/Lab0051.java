1678e5a46ff2a0b312903a1aa61b8a35fc4bd6dc
Sanjeev <82.sanjeev@gmail.com>
2024-01-26 14:22:41 +0530

    new program added

--- src/basic06/Lab0051.java
+++ src/basic06/Lab0051.java
@@ -1,8 +1,21 @@
 package src.basic06;
 
+import java.util.Scanner;
+
 public class Lab0051 {
     public static void main(String[] args) {
-        int arr2= new int [2][3];
+        //reverse value
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the a ");
+        int a = sc.nextInt();
+        System.out.println("Enter the b");
+        int b=sc.nextInt();
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("Value of a -->"+a);
+        System.out.println("Value of b --->"+b);
+
 
     }
 }
